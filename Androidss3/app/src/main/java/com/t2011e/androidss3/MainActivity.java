7b9102d651785bb62aed16dde1cf3cf8797b69ae
BOMER1 <anhdtth2010054@fpt.edu.vn>
2023-02-19 22:10:01 +0700

    first commit

---
+++ Androidss3/app/src/main/java/com/t2011e/androidss3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.t2011e.androidss3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
